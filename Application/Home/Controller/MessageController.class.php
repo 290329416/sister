639581668dc3e0146898b62947e686bc58e65a7b
sae <zhaoqingyang@lianchuangbrothers.com>
2015-11-27 18:00:14 +0800

    aaaa

--- Application/Home/Controller/MessageController.class.php
+++ Application/Home/Controller/MessageController.class.php
@@ -24,8 +24,33 @@ class MessageController extends IndexController {
         $this->Verify->entry();
     }
     public function add(){
-        // return I('post.');exit;
-        echo 1;exit;
-
+        
+        if(!$this->Verify->check(I('post.verifyCode'))){
+            echo  '{"result":"0","msg":"验证码错误!"}';exit;
+        }
+        if(empty(I('post.prisename'))){
+            echo  '{"result":"0","msg":"企业名称不能为空!"}';exit;
+        }
+        if(empty(I('post.name'))){
+            echo  '{"result":"0","msg":"联系人不能为空!"}';exit;
+        }
+        if(empty(I('post.phone')) | !is_numeric(I('post.phone'))){
+            echo  '{"result":"0","msg":"电话不合法!"}';exit;
+        }
+        if(empty(I('post.email')) | !preg_match("/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/", I('post.email'))){
+            echo  '{"result":"0","msg":"邮箱不合法!"}';exit;
+        }
+        if(empty(I('post.message'))){
+            echo  '{"result":"0","msg":"内容不能为空!"}';exit;
+        }
+        $message = M('message');
+        if($message -> create(I('post'))){
+            $result = $message->add();
+            if($result){
+                exit('1');
+            }
+        }
+        
+        exit('0');
     }
 }
